f746388a488f07798aee2315aed22abce5f29580
Vincent Perrin <vincent.perrin@gmail.com>
2017-11-30 22:23:02 +0100

    with Alexa

--- app.js
+++ app.js
@@ -20,14 +20,22 @@ var express = require('express'); // app server
 var bodyParser = require('body-parser'); // parser for post requests
 var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
 
+const redis = require('redis'); //To store conversation context
+
 var app = express();
 
 // Bootstrap application settings
 app.use(express.static('./public')); // load UI from public folder
 app.use(bodyParser.json());
 
+// Using some globals for now
+let conversation;
+let redisClient;
+let context;
+let Wresponse;
+
 // Create the service wrapper
-var conversation = new Conversation({
+var conversation_Web = new Conversation({
   // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
   // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
   //'username': process.env.CONVERSATION_USERNAME,
@@ -52,7 +60,7 @@ app.post('/api/message', function(req, res) {
   };
 
   // Send the input to the conversation service
-  conversation.message(payload, function(err, data) {
+  conversation_Web.message(payload, function(err, data) {
     if (err) {
       return res.status(err.code || 500).json(err);
     }
@@ -91,5 +99,146 @@ function updateMessage(input, response) {
   response.output.text = responseText;
   return response;
 }
+
+// Alexa handle part
+
+// routes will go here
+
+function errorResponse(reason) {
+  return {
+    version: '1.0',
+    response: {
+      shouldEndSession: true,
+      outputSpeech: {
+        type: 'PlainText',
+        text: reason || 'An unexpected error occurred. Please try again later.'
+      }
+    }
+  };
+}
+
+function initClients() {
+  return new Promise(function(resolve, reject) {
+  // Connect a client to Watson Conversation
+  conversation = new ConversationV1({
+  //'username': process.env.CONVERSATION_USERNAME,
+  //'password': process.env.CONVERSATION_PASSWORD,
+      version_date: '2016-09-20'
+  });
+  console.log('Connected to Watson Conversation');
+
+    // Connect a client to Redis
+  redisClient = redis.createClient('REDIS_PORT_NEED_TO_BE_REPLACED', 'REDIS_URL_NEED_TO_BE_REPLACED');
+  redisClient.auth('NEED_TO_BE_REPLACED', function (err) {
+      if (err) throw err;
+  });
+  redisClient.on('connect', function() {
+      console.log('Connected to Redis');
+  });
+  resolve("Done");
+});
+}
+
+function conversationMessage(request, workspaceId) {
+  return new Promise(function(resolve, reject) {
+    const input = request.intent ? request.intent.slots.EveryThingSlot.value : 'start skill';
+      var test = {
+          input: { text: input },
+          workspace_id: workspaceId,
+          context: context
+        };
+    console.log("Input" + JSON.stringify(test,null,2));
+    conversation.message(
+      {
+        input: { text: input },
+        workspace_id: workspaceId,
+        context: context
+      },
+      function(err, watsonResponse) {
+        if (err) {
+          console.error(err);
+          reject('Error talking to Watson.');
+        } else {
+          console.log(watsonResponse);
+          context = watsonResponse.context; // Update global context
+          
+          resolve(watsonResponse);
+        }
+      }
+    );
+  });
+}
+
+function getSessionContext(sessionId) {
+  console.log('sessionId: ' + sessionId);
+
+  return new Promise(function(resolve, reject) {
+    redisClient.get(sessionId, function(err, value) {
+      if (err) {
+        console.error(err);
+        reject('Error getting context from Redis.');
+      }
+      // set global context
+      context = value ? JSON.parse(value) : {};
+      console.log('Context Recupéré:');
+      console.log(context);
+      resolve();
+    });
+  });
+}
+
+function saveSessionContext(sessionId) {
+  console.log('Begin saveSessionContext');
+  console.log(sessionId);
+
+  // Save the context in Redis. Can do this after resolve(response).
+  if (context) {
+    const newContextString = JSON.stringify(context);
+    // Saved context will expire in 600 secs.
+    redisClient.set(sessionId, newContextString, 'EX', 600);
+    console.log('Saved context in Redis');
+    console.log(sessionId);
+    console.log(newContextString);
+  }
+}
+
+function sendResponse(response, resolve) {
+
+  // Combine the output messages into one message.
+  const output = response.output.text.join(' ');
+  var resp = {
+      version: '1.0',
+      response: {
+        shouldEndSession: false,
+        outputSpeech: {
+          type: 'PlainText',
+          text: output
+        }
+      }
+    };
+
+  Wresponse =  resp;
+  // Resolve the main promise now that we have our response
+  resolve(resp);
+}
+
+app.post('/api/alexa', function(args, res) {
+  return new Promise(function(resolve, reject) {
+    const request = args.body.request;
+    const sessionId = args.body.session.sessionId;
+    initClients()
+    .then(() => getSessionContext(sessionId))
+    .then(() => conversationMessage(request, '<workspace-id>'))
+    .then(actionResponse => sendResponse(actionResponse, resolve))
+    .then(data => {
+      res.json(Wresponse);
+  })
+  .then(() => saveSessionContext(sessionId))    
+  .catch(function (err) {
+      console.error('Erreur !');
+      console.dir(err);
+  });
+  });
+});
 
 module.exports = app;
